41dab699579b0395e7286f9e2774a4c9a27904b9
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-01-31 10:53:28 +0100

    introduces credo and removing it's warnings

--- lib/TimeA.ex
+++ lib/TimeA.ex
@@ -1,29 +1,37 @@
 defmodule TimeA do
+  @moduledoc """
+  """
 
+  @spec add_to_time(time1, time2) :: time
   def add_to_time(time1, time2) do
-   second   = rem ( time1.second + time2.second ), 60
-   e_second = div ( time1.second + time2.second ), 60
-   minute   = rem ( time1.minute + time2.minute + e_second ), 60
-   e_minute = div ( time1.minute + time2.minute + e_second ), 60
+   second   = rem (time1.second + time2.second), 60
+   e_second = div (time1.second + time2.second), 60
+   minute   = rem (time1.minute + time2.minute + e_second), 60
+   e_minute = div (time1.minute + time2.minute + e_second), 60
    hour = time1.hour +  time2.hour + e_minute
    {:ok, elapsed_time} = Time.new(hour, minute, second, 0)
    elapsed_time
   end
 
+  @spec get_bounding_time(start_time, duration) :: time
   def get_bounding_time(start_time, duration) do
     %{start_time: start_time, end_time: add_to_time(start_time, duration)}
   end
 
+  @spec overlap?(time1, time2) :: boolean
   def overlap?(nil, _), do: :false
   def overlap?(_, nil), do: :false
   def overlap?(time1, time2) do
-    if time1.end_time   >= time2.start_time && time1.end_time   <= time2.end_time do
+    if time1.end_time >= time2.start_time
+       && time1.end_time <= time2.end_time do
       :true
     else
-      if time1.start_time >= time2.start_time && time1.start_time <= time2.end_time do
+      if time1.start_time >= time2.start_time
+         && time1.start_time <= time2.end_time do
         :true
       else
-        if time1.start_time < time2.start_time && time1.end_time > time2.end_time do
+        if time1.start_time < time2.start_time
+           && time1.end_time > time2.end_time do
           :true
         else
           :false
@@ -32,10 +40,12 @@ defmodule TimeA do
     end
   end
 
+  @spec inRange?(time1, time2) :: boolean
   def inRange?(nil, _), do: :false
   def inRange?(_, nil), do: :false
   def inRange?(time1, time2) do
-    if time1.start_time >= time2.start_time && time1.end_time <= time2.end_time do
+    if time1.start_time >= time2.start_time
+       && time1.end_time <= time2.end_time do
       :true
     else
       :false
--- lib/schedule_create.ex
+++ lib/schedule_create.ex
@@ -1,61 +1,83 @@
 defmodule Schedule do
-
+  @moduledoc """
+  """
   require TimeA
 
   # an option for general unavailability should be added. like a class named *
-  @config_params [
-                   unavailable_classes:       [%{name: "a", time: %{start_time: ~T[12:00:00], end_time: ~T[17:00:00]}} ],
-                   unavailable_times:         [%{name: "lunch_time", time: %{start_time: ~T[12:00:00], end_time: ~T[13:30:00]}} ],
-                   class_strict_availability: [%{name: "b", time: %{start_time: ~T[09:00:00], end_time: ~T[12:00:00]}} ]
-                 ]
+  @config_params
+   [
+    unavailable_classes: [
+      %{name: "a", time: %{start_time: ~T[12:00:00], end_time: ~T[17:00:00]}}
+      ],
+    unavailable_times:
+    [%{name: "lunch_time",
+       time: %{start_time: ~T[12:00:00],
+       end_time: ~T[13:30:00]}}],
+    class_strict_availability:
+    [%{name: "b", time: %{start_time: ~T[09:00:00], end_time: ~T[12:00:00]}}]
+  ]
 
-  @input [ %{name: "a", duration: ~T[01:00:00]}, %{name: "b", duration: ~T[01:30:00]}, %{name: "c", duration: ~T[01:45:00]} , %{name: "d", duration: ~T[02:00:00]}]
+  @input
+  [
+    %{name: "a", duration: ~T[01:00:00]},
+    %{name: "b", duration: ~T[01:30:00]},
+    %{name: "c", duration: ~T[01:45:00]},
+    %{name: "d", duration: ~T[02:00:00]}
+  ]
 
   # run with no Configuration. a sample to just practice parsing of the data
+  @spec run(start_time, class_data) :: list
    def run(start_time, class_data) do
-     Enum.map(class_data, fn(class)->
-       add_to_list(start_time, class, class_data, [])
+     Enum.map(class_data, fn(class) ->
+       start_time
+       |> add_to_list(class, class_data, [])
        |> redundant_tree([])
        |> flatten_tree([])
        |> remove_nil_options
        |> print_schedule
      end)
-
    end
 
+   @spec flatten_tree(redundant_data, output) :: list
    def flatten_tree(redundant_data, output) do
      if is_list(redundant_data) && !is_map(List.first(redundant_data)) do
-       Enum.reduce(redundant_data, [], fn(item, data)->
+       Enum.reduce(redundant_data, [], fn(item, data) ->
          flatten_tree(item, output) ++ data
        end)
      else
-        [ %{options: List.flatten(Enum.reverse(redundant_data))} | output]
+        [%{options: List.flatten(Enum.reverse(redundant_data))} | output]
      end
    end
 
+   @spec redundant_tree(tree, parent) :: list
    def redundant_tree([], parent), do: parent
    def redundant_tree("", parent), do: parent
     def redundant_tree([head | tail], parent) do
        if is_list(head) do
          redundant_tree(head, List.flatten(parent))
        else
-         Enum.map(tail, fn(x)-> redundant_tree(x, [head | parent]) end)
+         Enum.map(tail, fn(x) -> redundant_tree(x, [head | parent]) end)
        end
    end
 
+   @spec remove_nil_options(flattened_list) :: list
    def remove_nil_options(flattened_list) do
-     Enum.reject(flattened_list, fn(item)-> Enum.any?(item.options, fn(x)-> is_nil(x) end) end)
+     Enum.reject(flattened_list, fn(item) ->
+                                  Enum.any?(item.options,fn(x) -> is_nil(x) end)
+                                 end)
    end
 
+   @spec print_schedule(flattened_tree) :: list
    def print_schedule(flattened_tree) do
-     Enum.map(flattened_tree, fn(x)->
-       Enum.map(x.options, fn(option)->
+     Enum.map(flattened_tree, fn(x) ->
+       Enum.map(x.options, fn(option) ->
          IO.puts "#{option.name} : #{option.start_time} - #{option.end_time}"
        end)
        IO.puts "*****************************************************"
      end)
    end
 
+   @spec add_to_list(current_time, class, remaining_classes, data) :: list
    def add_to_list(current_time, class, remaining_classes, data) do
      unique_list = List.delete(remaining_classes, class)
      bounding_time =
@@ -63,7 +85,10 @@ defmodule Schedule do
        |> TimeA.get_bounding_time(class.duration)
        |> get_next_available_time(class, @config_params)
 
-     calculated_class = %{name: class.name, duration: class.duration, start_time: bounding_time.start_time, end_time: bounding_time.end_time}
+     calculated_class = %{name: class.name,
+                          duration: class.duration,
+                          start_time: bounding_time.start_time,
+                          end_time: bounding_time.end_time}
      output = meet_conditions(calculated_class, bounding_time, @config_params)
 
      if Enum.empty?(unique_list) do
@@ -71,13 +96,20 @@ defmodule Schedule do
      else
        [
          output |
-         Enum.map(unique_list, fn(u_class)-> add_to_list(bounding_time.end_time, u_class, unique_list, data) end)
+         Enum.map(unique_list, fn(u_class) ->
+                                 add_to_list(bounding_time.end_time,
+                                             u_class,
+                                             unique_list,
+                                             data)
+                               end)
        ]
      end
    end
 
+   @spec meet_conditions(class, class_time, config_params) :: list
    def meet_conditions(class, class_time, config_params) do
-     if class_available?(class, class_time, config_params) && class_strict?(class, class_time, config_params) do
+     if class_available?(class, class_time, config_params)
+        && class_strict?(class, class_time, config_params) do
        class
      else
        [nil]
@@ -85,10 +117,11 @@ defmodule Schedule do
    end
 
    # just finds the first occurance
+   @spec class_strict?(class, class_time, config_params) :: boolean
    def class_strict?(class, class_time, config_params) do
      matched_class =
        config_params[:class_strict_availability]
-         |> Enum.find(fn(item)-> item.name == class.name end)
+         |> Enum.find(fn(item) -> item.name == class.name end)
      if is_nil(matched_class) do
        :true
      else
@@ -97,10 +130,11 @@ defmodule Schedule do
    end
 
    # just finds the first occurance
+   @spec class_available?(class, class_time, config_params) :: boolean
    def class_available?(class, class_time, config_params) do
      matched_class =
        config_params[:unavailable_classes]
-         |> Enum.find(fn(item)-> item.name == class.name end)
+         |> Enum.find(fn(item) -> item.name == class.name end)
      if is_nil(matched_class) do
        :true
      else
@@ -109,19 +143,24 @@ defmodule Schedule do
    end
 
    #assumes the config param is sorted
+   @spec need_shift_time?(current_time, config_params) :: boolean
    def need_shift_time?(current_time, config_params) do
      config_params[:unavailable_times]
-     |> Enum.map(fn(item)-> {TimeA.overlap?(current_time, item.time), item.time} end)
-     |> Enum.find(fn(item)-> match?({:true, _}, item)  end)
+       |> Enum.map(fn(item) ->
+                   {TimeA.overlap?(current_time, item.time), item.time} end)
+       |> Enum.find(fn(item) -> match?({:true, _}, item)  end)
    end
 
+   @spec get_next_available_time(current_time, class, config_params) :: time
    def get_next_available_time(current_time, class, config_params) do
      case need_shift_time?(current_time, config_params) do
-       {:true,  config_time} -> TimeA.get_bounding_time(config_time.end_time, class.duration)
+       {:true,  config_time} -> TimeA.get_bounding_time(config_time.end_time,
+                                                        class.duration)
        _                     -> current_time
      end
    end
 
+   @spec sort_config_params(config_params) :: list
    def sort_config_params(config_params) do
     config_params
    end
